aae8c86f0feaee33cdf4005415ee083772d4f76f
jidonghao <ji534@outlook.com>
2022-07-06 19:36:11 +0800

    导航栏

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const collapsiables = document.querySelectorAll(".collapsiable .collapsiable__header")
+const collapsiables = document.querySelectorAll(".collapsiable")
 collapsiables.forEach(item => {
     item.onclick=function(){
-        this.parentNode.classList.toggle("collapsiable--expanded")
+        this.classList.toggle("collapsiable--expanded")
     }
 });
